0cdf47dadae1d9902071e395d6056d4680dd9b8c
irahayuningtias <2031710085@student.polinema.ac.id>
2021-10-27 17:24:23 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Makam Bung Karno</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap Icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- SimpleLightbox plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">Sistem Informasi Makam Bung Karno Blitar</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#services">Navigation</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#documentation">Pin Location</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                        <h1 class="text-white font-weight-bold">Makam Bung Karno</h1>
+                        <hr class="divider" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 mb-5">Wisata Populer dan Bersejarah di Kota Blitar</p>
+                        <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        
+        <!-- About-->
+        <section class="page-section bg-primary" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">Tentang Makam Bung Karno</h2>
+                        <hr class="divider divider-light" />
+                        <p class="text-white-75 mb-4">Makam Bung Karno merupakan kompleks pemakaman presiden pertama Republik Indonesia, Soekarno.
+                            Desain dari pemakaman ini dibangun dengan arsitektur khas Jawa, yaitu bangunan joglo. 
+                            Kompleks tersebut terletak di Bendogerit, Sananwetan, Blitar, dan dibangun di akhir 1970-an. 
+                            Ratusan ribu peziarah, baik rohani maupun politik, mengunjungi makam tersebut setiap tahun. 
+                            Kompleks ini memiliki museum, perpustakaan nasional, teater, gong perdamaian, makam sang proklamator,
+                            dan pasar oleh-oleh.<br><br>
+                            Buka : Setiap Hari<br>
+                            Jam : 07.00 - 17.00 WIB<br>
+                            Alamat : Jl. Ir. Soekarno No.152, Bendogerit, Kec. Sananwetan, Kota Blitar, Jawa Timur 66133<br>
+                            Tiket : Gratis<br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">Navigasi Menuju ke MBK</h2>
+                <hr class="divider" />
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15800.647723813197!2d112.1764575!3d-8.0849629!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3e8ddfeb7e6b3d02!2sMakam%20Bung%20Karno!5e0!3m2!1sid!2sid!4v1635300527660!5m2!1sid!2sid" 
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
+        </section>
+
+        <!-- Pin Location-->
+            <div id="documentation">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <br><br>
+                        <h2 class="text-black mt-0">Kompleks Makam Bung Karno</h2>
+                        <hr class="divider" /> 
+                    </div>
+                <div class="container-fluid p-0">
+                <div class="row g-0">    
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="https://g.page/makam-bung-karno-blitar?share" title="Makam Soekarno">
+                            <img class="img-fluid" src="../img/Bung-Karno-03-1024x536.jpg" width="400" height="200" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-name">Makam Soekarno</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="https://goo.gl/maps/3fRszFbTeqkCuXHw5" title="Perpusnas Bung Karno">
+                            <img class="img-fluid" src="../img/museum-bung-karno_169.jpeg" width="400" height="200" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-name">Perpustakaan Nasional Proklamator Bung Karno</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="https://goo.gl/maps/T2C54zJAJC5wrXm27" title="Gong Perdamaian">
+                            <img class="img-fluid" src="../img/gong-perdamaian-di-kompleks.jpg" width="400" height="200" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-name">Gong Perdamaian Dunia</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="https://g.page/perpusbungkarno?share" title="Museum">
+                            <br>
+                            <img class="img-fluid" src="../img/museum.jpg" width="400" height="200" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-name">Museum MBK</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="../img/teater.jpg" title="teater">
+                        <br>
+                            <img class="img-fluid" src="../img/teater.jpg" width="400" height="200" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-name">Teater MBK</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <br>
+                        <a class="portfolio-box" href="../img/Pasar-Makam-Bung-Karno.jpg" title="pasar">
+                            <img class="img-fluid" src="../img/Pasar-Makam-Bung-Karno.jpg" width="400" height="200" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-name">Pasar Oleh-Oleh MBK</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+                </div>
+            </div>
+        </section>
+        
+        <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 col-xl-6 text-center">
+                        <br><br><br><br><br><br><br><br><br><br><br>
+                        <h2 class="mt-0">Kontak Kami</h2>
+                        <hr class="divider" />
+                        <p> Alamat: Jl. Ir. Soekarno No.152, Bendogerit, Kec. Sananwetan, Kota Blitar, Jawa Timur 66133<br>
+                        Telepon: (0342) 801145<br>
+                        Email: makambungkarnoblitar@gmail.com<br>
+                    </div>
+                </div>
+                
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Indah Rahayuningtias - MI2D/16/2031710085 <br>
+                D-III Manajemen Informatika Politeknik Negeri Malang
+        </div></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- SimpleLightbox plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
